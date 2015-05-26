130bf56fc8eb8ac62da8010fa204f85c89a7095f
Christiane Kammerl <christiane@kammerl.com>
2015-05-25 19:39:32 -0700

    refactores

--- public/services.js
+++ public/services.js
@@ -1,7 +1,6 @@
 angular.module('sudoku')
 
 .factory('Board', function() {
-
   var create = function() {
     var board = new Array();
     board.push( [1, 2, 3], [null, null, null], [2, null, null] );
