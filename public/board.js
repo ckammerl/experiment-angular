64ba7739866666b3674ee21a72393c833024baea
Christiane Kammerl <christiane@kammerl.com>
2015-05-26 11:55:07 -0700

    moves logic of getting row and col values and of validating the entered number from form.js to board.js

--- public/board.js
+++ public/board.js
@@ -14,14 +14,62 @@ angular.module('sudoku')
     $state.go('form', {row: clickedRow, col: clickedCol});
   };
 
-  // console.log('stateParams', $stateParams.valid);
+  $scope.isValid = function(row, col) {
+    var currentRow = getRowValues(row);
+    var currentCol = getColValues(col);
+    var valid = isValid(currentRow, currentCol);
 
-  $scope.valid = $stateParams.valid;
+    if ( !valid ) {
+      return 'notvalid'
+    }
 
-  // $scope.isValid = function(row, col) {
+    return 'valid';
+  };
 
+  var getRowValues = function(clickedRowIdx) {
+    var rowValues = {};
 
-  // }
+    for ( var i = 0; i < $scope.board.length; i++ ) {
+      if ( !rowValues.hasOwnProperty($scope.board[clickedRowIdx][i]) ) {
+        rowValues[$scope.board[clickedRowIdx][i] ] = true;
+      } else if ( rowValues.hasOwnProperty($scope.board[clickedRowIdx][i]) && $scope.board[clickedRowIdx][i] === null ) {
+        rowValues[$scope.board[clickedRowIdx][i]] = true;
+      } else {
+        rowValues[$scope.board[clickedRowIdx][i]] = false;
+      }
+    }
+    return rowValues;
+  };
+
+  var getColValues = function(clickedColIdx) {
+    var colValues = {};
+
+    for ( var i = 0; i < $scope.board.length; i++ ) {
+      if ( !colValues.hasOwnProperty($scope.board[i][clickedColIdx]) ) {
+        colValues[$scope.board[i][clickedColIdx]] = true;
+      } else if ( colValues.hasOwnProperty($scope.board[i][clickedColIdx]) && $scope.board[i][clickedColIdx] === null ) {
+        colValues[$scope.board[i][clickedColIdx]] = true;
+      } else {
+        colValues[$scope.board[i][clickedColIdx]] = false;
+      }
+    }
+    return colValues;
+  };
+
+  var isValid = function(rowValues, colValues) {
+    for (var key in rowValues) {
+      if (rowValues[key] === false ) {
+        return false;
+      }
+    }
+
+    for (var key in colValues) {
+      if (colValues[key] === false) {
+        return false;
+      }
+    }
+    return true;
+  };
 
   $scope.$watch('board', function() {
     console.log('board', $scope.board);
--- public/form.js
+++ public/form.js
@@ -10,65 +10,9 @@ angular.module('sudoku')
     var col = $stateParams.col;
 
     var oldCellValue = $scope.board[row][col];
-    // set new value into cell
+    // set new value into clicked cell
     $scope.board[row][col] = $scope.number;
 
-    var currentRow = getRowValues(row);
-    var currentCol = getColValues(col);
-
-    var valid = isValid(currentRow, currentCol);
-
-    console.log("valid", valid);
-
-    if ( !valid ) {
-      $scope.board[row][col] = oldCellValue;
-    }
-
-    $state.go('board', {valid: valid});
-  };
-
-  var getRowValues = function(clickedRowIdx) {
-    var rowValues = {};
-
-    for ( var i = 0; i < $scope.board.length; i++ ) {
-      if ( !rowValues.hasOwnProperty($scope.board[clickedRowIdx][i]) ) {
-        rowValues[$scope.board[clickedRowIdx][i] ] = true;
-      } else if ( rowValues.hasOwnProperty($scope.board[clickedRowIdx][i]) && $scope.board[clickedRowIdx][i] === null ) {
-        rowValues[$scope.board[clickedRowIdx][i]] = true;
-      } else {
-        rowValues[$scope.board[clickedRowIdx][i]] = false;
-      }
-    }
-    return rowValues;
-  };
-
-  var getColValues = function(clickedColIdx) {
-    var colValues = {};
-
-    for ( var i = 0; i < $scope.board.length; i++ ) {
-      if ( !colValues.hasOwnProperty($scope.board[i][clickedColIdx]) ) {
-        colValues[$scope.board[i][clickedColIdx]] = true;
-      } else if ( colValues.hasOwnProperty($scope.board[i][clickedColIdx]) && $scope.board[i][clickedColIdx] === null ) {
-        colValues[$scope.board[i][clickedColIdx]] = true;
-      } else {
-        colValues[$scope.board[i][clickedColIdx]] = false;
-      }
-    }
-    return colValues;
-  };
-
-  var isValid = function(rowValues, colValues) {
-    for (var key in rowValues) {
-      if (rowValues[key] === false ) {
-        return false;
-      }
-    }
-
-    for (var key in colValues) {
-      if (colValues[key] === false) {
-        return false;
-      }
-    }
-    return true;
+    $state.go('board', {row: row, col: col});
   };
 });
